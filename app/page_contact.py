6653868f6818eedc8883d423ae619961aa54c3d1
jerry <jerrykuo1016@gmail.com>
2025-10-17 15:13:05 +0800

    主頁設計

--- app/page_contact.py
+++ app/page_contact.py
@@ -7,4 +7,4 @@ templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/contact", response_class=HTMLResponse)
 async def contact(request: Request):
-    return templates.TemplateResponse("contact.html", {"request": request})
+    return templates.TemplateResponse("contact.html", {"request": request,  "name": "Jerry Kuo"})
